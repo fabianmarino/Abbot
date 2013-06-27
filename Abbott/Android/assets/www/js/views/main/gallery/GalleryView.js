7a44074515a19ced2dcf2b49d78c2ed2a1e828f4
Fabian <fabian@appsolution.co>
2013-06-27 14:10:44 -0500

    quitando conflictos head

--- Abbott/Android/assets/www/js/views/main/gallery/GalleryView.js
+++ Abbott/Android/assets/www/js/views/main/gallery/GalleryView.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 GalleryView = apps.ui.View.extend({
 
     initialize : function(options) {
@@ -29,35 +28,3 @@ GalleryView = apps.ui.View.extend({
         this.parent.setView('creategallery');
     }
 });
-=======
-GalleryView = apps.ui.View.extend({
-
-    initialize : function(options) {
-        apps.ui.View.prototype.initialize.call(this, options);
-
-        this.prefix = 'GalleryView';
-        util.log(this.prefix, 'initialize()');
-
-        this.el = $($('#GalleryTemplate', App.templates).html());
-
-        this.parent = options.parent;
-        this.delegateEvents();
-
-        this.activated = false;
-    },
-    
-    events : {
-        'touchend #createGallery' : 'onCreateGallery'
-    },
-
-    activate : function() {
-        if (this.activated == false) {
-            this.activated = true;
-        }
-    },
-    
-    onCreateGallery : function(){
-        this.parent.setView('creategallery');
-    }
-});
->>>>>>> a3d6ec2178452f51a85417da11e173d3ade4e94a
