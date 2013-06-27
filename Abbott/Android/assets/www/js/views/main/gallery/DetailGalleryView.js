e9187dc29ef9f275f1d4a82effa07486c079ed5f
Fabian <fabian@appsolution.co>
2013-06-27 14:10:22 -0500

    quitando conflictos head

--- Abbott/Android/assets/www/js/views/main/gallery/DetailGalleryView.js
+++ Abbott/Android/assets/www/js/views/main/gallery/DetailGalleryView.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 DetailGalleryView = apps.ui.View.extend({
 
     initialize : function(options) {
@@ -25,31 +24,4 @@ DetailGalleryView = apps.ui.View.extend({
         }
     }
 });
-=======
-DetailGalleryView = apps.ui.View.extend({
 
-    initialize : function(options) {
-        apps.ui.View.prototype.initialize.call(this, options);
-
-        this.prefix = 'DetailGalleryView';
-        util.log(this.prefix, 'initialize()');
-
-        this.el = $($('#DetailGalleryTemplate', App.templates).html());
-
-        this.parent = options.parent;
-        this.delegateEvents();
-
-        this.activated = false;
-    },
-    
-    events : {
-
-    },
-
-    activate : function() {
-        if (this.activated == false) {
-            this.activated = true;
-        }
-    }
-});
->>>>>>> a3d6ec2178452f51a85417da11e173d3ade4e94a
