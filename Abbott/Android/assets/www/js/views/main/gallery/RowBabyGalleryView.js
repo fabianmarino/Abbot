b6bf6edb31199b74211533007c4a8ea5be514866
Fabian <fabian@appsolution.co>
2013-06-27 14:11:19 -0500

    quitando conflictos head

--- Abbott/Android/assets/www/js/views/main/gallery/RowBabyGalleryView.js
+++ Abbott/Android/assets/www/js/views/main/gallery/RowBabyGalleryView.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 RowBabyGalleryView = apps.ui.View.extend({
 
     initialize : function(options) {
@@ -29,35 +28,3 @@ RowBabyGalleryView = apps.ui.View.extend({
         this.$('#babyGallery').attr('onClick', 'App.pages.views.content.contentManager.views.mybaby.nextView('+this.idGallery+');');
     }
 });
-=======
-RowBabyGalleryView = apps.ui.View.extend({
-
-    initialize : function(options) {
-        apps.ui.View.prototype.initialize.call(this, options);
-
-        this.prefix = 'RowBabyGalleryView';
-        util.log(this.prefix, 'initialize()');
-        
-        this.idGallery=options.idGallery;
-        this.nameGallery=options.nameGallery;
-        this.dateGallery=options.dateGallery;
-        this.photoGallery=options.photoGallery;
-        
-        this.el = $($('#rowBabyGalleryTemplate', App.templates).html());
-
-        this.delegateEvents();
-        
-        this.$('#babyGalleryImg').css({'width':'100px', 'height':'100px'});
-        this.render();
-    },
-
-    render : function(){
-        if(this.photoGallery){
-            this.$('#babyGalleryImg').css({'background-image' : 'url(' + this.photoGallery + ')'});
-        }
-        this.$('#babyGalleryDate').text(this.dateGallery);
-        this.$('#babyGalleryTitle').text(this.nameGallery);
-        this.$('#babyGallery').attr('onClick', 'App.pages.views.content.contentManager.views.mybaby.nextView('+this.idGallery+');');
-    }
-});
->>>>>>> a3d6ec2178452f51a85417da11e173d3ade4e94a
